47a4df80fc0c84f66d5b66a8741875a160fd1d6e
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-05-28 01:25:53 +0500

    add todo CRUD

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const connectToDatabase = require("./config/database");
+const Todo = require("./models/todo");
 
 const app = express();
 app.use(express.json());
@@ -12,8 +13,60 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 connectToDatabase();
 
+// Root route
 app.get("/", (req, res) => {
   res.json({ info: "API routes at /api/v1/todos" }).status(200);
+});
+
+// Get all todos
+app.get("/api/v1/todos", async (req, res) => {
+  const todos = await Todo.find()
+    .then((data) => res.json(data))
+    .catch((err) => console.log(err));
+});
+
+// Get todo by its id
+app.get("/api/v1/todos/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id)
+    .then((data) => res.json(data).status(200))
+    .catch((err) => console.log(err));
+});
+
+// Create a todo task
+app.post("/api/v1/todos", async (req, res) => {
+  const todo = new Todo(req.body);
+
+  await todo
+    .save()
+    .then(() => res.json(todo).status(201))
+    .catch((err) => console.log(err));
+});
+
+// Toggle todo task completion
+app.patch("/api/v1/todos/:id", async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  todo.completed = !todo.completed;
+
+  await todo
+    .save()
+    .then(() => res.json(todo).status(200))
+    .catch((err) => console.log(err));
+});
+
+// Update a todo task
+app.put("/api/v1/todos/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.json(todo).status(200);
+});
+
+// Delete a todo task
+app.delete("/api/v1/todos/:id", async (req, res) => {
+  const todo = await Todo.findByIdAndDelete(req.params.id);
+
+  res.json({ message: "Todo task deleted", data: todo }).status(200);
 });
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
