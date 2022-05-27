2b020da1229d637bda0e29e04b84ab8f8634507c
Qasim Abdullah <qasim.abdullah@travis-ci.org>
2022-05-28 00:35:03 +0500

    db connection

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const connectToDatabase = require("./config/database");
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 5000;
+connectToDatabase();
 
 app.get("/", (req, res) => {
   res.json({ info: "API routes at /api/v1/todos" }).status(200);
